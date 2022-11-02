30b466d2b1fc9f1cc146de35fc6fc8ce5acb3885
MArslan88 <gery_maz_555@hotmail.com>
2022-11-02 16:08:19 +0500

    send data into FIREBASE FIRESTORE

--- app/src/main/java/com/mhdarslan/inspiringquote/MainActivity.java
+++ app/src/main/java/com/mhdarslan/inspiringquote/MainActivity.java
@@ -1,26 +1,62 @@
 package com.mhdarslan.inspiringquote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button save_btn, load_btn;
+    TextView quoteTextView;
+
     public static final String QUOTE_KEY = "quote";
     public static final String AUTHOR_KEY = "author";
+    public static final String TAG = "inspirationQuote";
+
+    //    private DocumentReference mDocRef = FirebaseFirestore.getInstance().collection("sampleData").document("inspiration");
+    private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("sampleData/inspiration");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        save_btn = findViewById(R.id.save_btn);
+        load_btn = findViewById(R.id.load_btn);
+        quoteTextView = findViewById(R.id.quote_txt);
+
+        save_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveQuote();
+            }
+        });
+
+        load_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchQuote();
+            }
+        });
+
     }
 
-    public void saveQuote (View view){
+    public void saveQuote (){
         EditText quoteView = findViewById(R.id.quote_edt);
         EditText authorView = findViewById(R.id.author_edt);
 
@@ -32,5 +68,24 @@ public class MainActivity extends AppCompatActivity {
         Map<String,Object> dataToSave = new HashMap<String,Object>();
         dataToSave.put(QUOTE_KEY,quoteText);
         dataToSave.put(AUTHOR_KEY,authorText);
+
+        // send data into firebase
+        mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d(TAG,"Document has been saved");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG,"Document was not saved!",e);
+            }
+        });
+
+        quoteView.setText("");
+        authorView.setText("");
+    }
+
+    public void fetchQuote(){
     }
 }
